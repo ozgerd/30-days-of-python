2d85cdb76f7062a94c4933bd56e928a794b0054b
ozgerd <130622398+ozgerd@users.noreply.github.com>
2023-05-17 14:32:45 +0300

    Add files via upload

---
+++ 01-introduction.py
@@ -0,0 +1,113 @@
+# print ('Hello World')
+# print("Again, Hello World")
+# age = 20
+# price = 19.95
+# print (age)
+# print(price)
+
+# birthYear = input ('What is your birthday?')
+# age = 2023 - int (birthYear)
+# print (age)
+
+# question = input ("What is the meaning of life?")
+# meaning = question
+# print ('It is the question')
+
+# question = "The answer is 42"
+# print ('42' in question)
+
+# x = 3 > 2
+# print(x)
+
+# x = 3 == "3"
+# print (x)
+
+# price = 20
+# print (price > 10 and price < 30)
+# print ( price > 10, price == 11)
+
+# temperature = 14
+# if temperature > 18:
+#    print ('Heating is off')
+# elif temperature < 17:
+#    print ('Heating is on')
+# elif temperature < 15:
+#   print ('System Error')
+
+
+
+
+# name = input ("What is your name? ")
+# print ("Hello " + name )
+
+
+#
+
+# first = input ("Enter first number: ")
+# second = input ("Enter second number: ")
+
+# total = int (first) + int (second)
+# print (total)
+
+# first = input ('First: ')
+# second = input ('Last: ')
+# space = ' '
+# fullName = first + space second
+# print (fullName)
+   
+# test = "Python"
+# print ( test.replace('Python', 'Apex'))
+
+# course = "Apex for beginners"
+# print ("Apex" in course)
+# x = 4
+# x += 3
+# print(x)
+
+# y = 2
+# y -=1
+# print (y)
+
+# x = 3 > 2
+# print(x)
+
+# grade = 2
+# print (grade < 5 and grade > 3)
+# print (grade)
+# if (grade > 4):
+#   print ('pass')
+# elif (grade < 3):
+#   print ('failed')
+
+# weight = input ("Enter your weight: ")
+# height = input ("Enter your height: ")
+# unit = input ("(K)g or (L)bs: ")
+# if unit.upper()== weight / 0.45
+# print ("Weight in Lbs: ")
+# int (weight)
+# ibm = float (height) - int (weight)
+# print (ibm)
+ 
+
+# i = 1
+# while i <= 10:
+#     print (i * '*')
+#     i +=1
+
+
+
+# names = ['Arthur', "Tricia", "Ford"]
+# names [0] = "Zaphod"
+# print (names [0: 2])
+# print(names)
+
+# number = [1, 2, 3, 4, 5]
+# number.remove(3)
+# print(1 in number)
+# print(len(number))
+
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:
+    print(item)
+    
+
